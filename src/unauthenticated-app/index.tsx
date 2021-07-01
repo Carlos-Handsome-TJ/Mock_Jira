b09f8f977681e14116c20c5fd9a5380f01d5e75f
Carlos-Handsome-TJ <201821100479@smail.xtu.edu.cn>
2021-07-01 19:46:40 +0800

    modify: 增加页面样式。

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -1,14 +1,24 @@
 import React, {useState} from "react";
 import {useAuth} from "../context/auth-context";
-import {Form, Input, Button} from "antd";
-
+import {Form, Input, Button, Divider} from "antd";
+import styled from "@emotion/styled";
+import logo from "../assets/logo.svg"
+import left from "../assets/left.svg"
+import right from "../assets/right.svg"
 
 export const UnauthenticatedApp = () => {
     const [isRegister, setIsRegister] = useState(false)
-    return <>
-        {isRegister ? <RegisterModule/>: <LoginModule />}
-        <Button onClick={() => setIsRegister(!isRegister)}>{!isRegister ? "切换注册" : "切换到登录"}</Button>
-    </>
+    return (<StyleContainer>
+        <StyleLogo/>
+        <StyleLogin>
+            <StyleH2>请登录</StyleH2>
+            {isRegister ? <RegisterModule/> : <LoginModule/>}
+            <Divider/>
+            <Button type={"link"} onClick={() => setIsRegister(!isRegister)}>
+                {!isRegister ? "没有账号？注册新账号" : "已经有账号了？直接登录"}
+            </Button>
+        </StyleLogin>
+    </StyleContainer>)
 }
 /**
  * 登录模块
@@ -16,19 +26,19 @@ export const UnauthenticatedApp = () => {
  */
 const LoginModule = () => {
     const {login} = useAuth()
-    const handleSubmit = (value:{username:string, password:string}) => {
+    const handleSubmit = (value: { username: string, password: string }) => {
         login(value)
     }
     return <>
         <Form onFinish={handleSubmit}>
-            <Form.Item label={"用户名"} name={"username"}>
-                <Input />
+            <Form.Item name={"username"}>
+                <Input placeholder={"用户名"}/>
             </Form.Item>
-            <Form.Item label={"密码"} name={"password"}>
-                <Input.Password/>
+            <Form.Item name={"password"}>
+                <Input.Password placeholder={"密码"}/>
             </Form.Item>
             <Form.Item>
-                <Button htmlType="submit">登录</Button>
+                <StyleButton htmlType="submit">登录</StyleButton>
             </Form.Item>
         </Form>
     </>
@@ -39,20 +49,58 @@ const LoginModule = () => {
  */
 const RegisterModule = () => {
     const {register} = useAuth()
-    const handleSubmit = (value: {username: string, password: string}) => {
+    const handleSubmit = (value: { username: string, password: string }) => {
         register(value)
     }
-    return<>
+    return <>
         <Form onFinish={handleSubmit}>
-            <Form.Item label={"用户名"} name={"username"}>
-                <Input />
+            <Form.Item name={"username"}>
+                <Input placeholder={"用户名"}/>
             </Form.Item>
-            <Form.Item label={"密码"} name={"password"}>
-                <Input.Password />
+            <Form.Item name={"password"}>
+                <Input.Password placeholder={"密码"}/>
+            </Form.Item>
+            <Form.Item name={"confirm-password"}>
+                <Input.Password placeholder={"确认密码"}/>
             </Form.Item>
             <Form.Item>
-                <Button htmlType="submit">注册</Button>
+                <StyleButton htmlType="submit">注册</StyleButton>
             </Form.Item>
         </Form>
     </>
 }
+
+const StyleContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  background: url(${left}) no-repeat left bottom, url(${right}) no-repeat right bottom;
+  background-size: calc(((100vw - 40rem) / 2) - 3.2rem), calc(((100vw - 40rem) / 2) - 3.2rem), cover;
+
+`
+const StyleLogo = styled.header`
+  width: 5rem;
+  height: 5rem;
+  margin: 0 auto;
+  padding: 5rem;
+  background: url(${logo}) no-repeat center center;
+`
+const StyleH2 = styled("h2")`
+  text-align: center;
+  margin-bottom: 2.4rem;
+  font-weight: 500;
+`
+const StyleButton = styled(Button)`
+  width: 100%;
+  color: #fff;
+  background-color: #2372d9;
+  text-align: center;
+`
+const StyleLogin = styled.div`
+  width: 40rem;
+  height: 56rem;
+  margin: 0 auto;
+  padding: 3.2rem 4rem;
+  border: .2rem rgba(0, 0, 0, .1) solid;
+  box-shadow: rgb(0 0 0 / 10%) 0 0 10px;
+`
